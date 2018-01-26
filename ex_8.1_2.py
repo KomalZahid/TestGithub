092bbaa27d76fe00712c779f2046c75374e2fcee
KomalZahid <komalzahid94.kz@gmail.com>
2018-01-27 01:50:54 +0500

    Exercise 8.1 and 8.2

---
+++ ex_8.1_2.py
@@ -0,0 +1,14 @@
+#Exercise 8.1
+
+def display_message(message):
+    print(message)
+
+display_message('In this chapter you will study about functions.')
+
+###----------------------------------------###
+#Exercise 8.2
+def favourite_book(title):
+    print('One of my favourite book is ' + title.title())
+
+
+favourite_book('inaam')
